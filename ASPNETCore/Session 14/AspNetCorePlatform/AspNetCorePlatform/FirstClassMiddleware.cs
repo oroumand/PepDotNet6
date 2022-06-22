843efb269045b451b6822f9fe423c4a03cc11abe
Alireza Oroumand <ar.oroumand@gmail.com>
2022-06-22 18:33:35 +0430

    Session 19

---
+++ ASPNETCore/Session 14/AspNetCorePlatform/AspNetCorePlatform/FirstClassMiddleware.cs
@@ -0,0 +1,79 @@
+﻿namespace AspNetCorePlatform;
+
+public class FirstClassMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public FirstClassMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Query.ContainsKey("AddText"))
+        {
+            await context.Response.WriteAsync("First Class Executing. \n");
+        }
+        await _next(context);
+        if (context.Request.Query.ContainsKey("AddText"))
+        {
+            await context.Response.WriteAsync("First Class Executed. \n");
+        }
+
+    }
+
+}
+
+
+public class SeccondClassMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public SeccondClassMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        //context.Response.ContentType = "text/html";
+
+        int A = 123;
+        //await context.Response.WriteAsync("Seccond Class Executing. \n");
+
+        await _next(context);
+        int B = 12;
+
+        //await context.Response.WriteAsync("Seccond Class Executed. \n");
+
+
+    }
+
+}
+
+
+public class ThirdClassMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ThirdClassMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+
+        //await context.Response.WriteAsync("Third Class Executing. \n");
+        int A = 123;
+
+        await _next(context);
+        int B = 12;
+
+        //await context.Response.WriteAsync("Third Class Executed. \n");
+
+
+    }
+
+}
---
+++ ASPNETCore/Session 14/AspNetCorePlatform/AspNetCorePlatform/HostingExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace AspNetCorePlatform;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+       
+        return builder.Build();
+    }
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+
+        app.Map("/admin", myapp =>
+        {
+            myapp.UseMiddleware<SeccondClassMiddleware>();
+            myapp.UseMiddleware<ThirdClassMiddleware>();
+
+        });
+
+        app.Use(async (httpcontext, next) =>
+        {
+            if (httpcontext.Request.Query.ContainsKey("AddText"))
+            {
+                httpcontext.Response.ContentType = "text/html";
+                await httpcontext.Response.WriteAsync("Use Middleware executing \n");
+            }
+            if (!httpcontext.Request.Query.ContainsKey("Sh"))
+            {
+                await next();
+
+            }
+            if (httpcontext.Request.Query.ContainsKey("AddText"))
+            {
+                await httpcontext.Response.WriteAsync("Use Middleware executed ");
+            }
+        });
+
+
+        app.UseMiddleware<FirstClassMiddleware>();
+
+
+        app.MapGet("/", () => "Hello World!\n");
+
+        return app;
+    }
+}
